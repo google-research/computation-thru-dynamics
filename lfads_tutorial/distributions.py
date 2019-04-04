Put the LFADS example into a notebook to run. Found and fixed the bug in the nans.  They were related to the KL divergences, and were fixed by adding a minimum variance to all computations related to gaussian probability.

lfads_tutorial/distributions.py
@@ -22,7 +22,7 @@ import jax.numpy as np
 from jax import grad, jit, vmap
 from jax import random
 
-import utils
+import lfads_tutorial.utils as utils
 
 def poisson_log_likelihood(x, log_rate):
   """Compute the log likelihood under Poisson distribution
@@ -44,20 +44,22 @@ def poisson_log_likelihood(x, log_rate):
   return x * log_rate - np.exp(log_rate) #- np.lgamma(x + 1)
 
 
-def diag_gaussian_sample(key, mean, logvar):
+def diag_gaussian_sample(key, mean, logvar, varmin=1e-16):
   """x ~ N(mean, exp(logvar))
 
   Arguments:
     key: random.PRNGKey for random bits
     mean: np array, mean of gaussian
     logvar: np array, log variance of gaussian
+    varmin: minimum variance allowed, helps with numerical stability
 
   Returns:
     np array samples from the gaussian of the same size as mean and logvar"""
-  return mean + np.exp(0.5*logvar) * random.normal(key, mean.shape)
+  logvar_wm = np.log(np.exp(logvar) + varmin)
+  return mean + np.exp(0.5*logvar_wm) * random.normal(key, mean.shape)
 
 
-def diag_gaussian_log_likelihood(z, mean=0.0, logvar=0.0):
+def diag_gaussian_log_likelihood(z, mean=0.0, logvar=0.0, varmin=1e-16):
   """Log-likelihood under a Gaussian distribution with diagonal covariance.
      Returns the log-likelihood for each dimension.
 
@@ -65,15 +67,17 @@ def diag_gaussian_log_likelihood(z, mean=0.0, logvar=0.0):
     z: The value to compute the log-likelihood.
     mean: The mean of the Gaussian
     logvar: The log variance of the Gaussian.
+    varmin: Minimum variance allowed (numerically useful).
 
   Returns:
     The log-likelihood under the Gaussian model.
   """
+  logvar_wm = np.log(np.exp(logvar) + varmin)
   return (-0.5 * (logvar + np.log(2*np.pi) +
-                  np.square((z-mean)/np.exp(0.5*logvar))))
+                  np.square((z-mean)/( np.exp(0.5*(logvar_wm))))))
 
 
-def kl_gauss_gauss(z_mean, z_logvar, prior_params):
+def kl_gauss_gauss(z_mean, z_logvar, prior_params, varmin=1e-16):
   """Compute the KL divergence between two diagonal Gaussian distributions.
             KL(q||p) = E_q[log q(z) - log p(z))]
    Args:
@@ -81,24 +85,26 @@ def kl_gauss_gauss(z_mean, z_logvar, prior_params):
     z_logvar: logvar of posterior
     prior_z_mean: mean of prior, z ~ p(z)
     prior_z_logvar: logvar of prior
+    varmin: minimum variance allowed, useful for numerical stability
 
     Returns:
       np array of KL, computed analytically, same size as z_mean
   """
+  z_logvar_wm = np.log(np.exp(z_logvar) + varmin)
   prior_mean = prior_params['mean']
-  prior_logvar = prior_params['logvar']
-  return (0.5 * (prior_logvar - z_logvar
-                 + np.exp(z_logvar - prior_logvar)
-                 + np.square((z_mean - prior_mean) / np.exp(0.5 * prior_logvar))
+  prior_logvar_wm = np.log(np.exp(prior_params['logvar']) + varmin)
+  return (0.5 * (prior_logvar_wm - z_logvar_wm
+                 + np.exp(z_logvar_wm - prior_logvar_wm)
+                 + np.square((z_mean - prior_mean) / np.exp(0.5 * prior_logvar_wm))
                  - 1.0))
 
 
 # This function is used in the loss, which is already batch aware. But the
 # prior parameters are the same across batch, thus in_axes below.
-batch_kl_gauss_gauss = vmap(kl_gauss_gauss, in_axes=(0, 0, None))
+batch_kl_gauss_gauss = vmap(kl_gauss_gauss, in_axes=(0, 0, None, None))
 
 
-def kl_gauss_ar1(key, z_mean_t, z_logvar_t, ar1_params):
+def kl_gauss_ar1(key, z_mean_t, z_logvar_t, ar1_params, varmin=1e-16):
   """KL using samples for multi-dim gaussian (thru time) and AR(1) process.
   To sample KL(q||p), we sample
         ln q - ln p
@@ -110,6 +116,7 @@ def kl_gauss_ar1(key, z_mean_t, z_logvar_t, ar1_params):
     z_mean_t: np.array of means with leading dim being time
     z_logvar_t: np.array of log vars, leading dim is time
     ar1_params: dictionary of ar1 parameters, log noise var and autocorr tau
+    varmin: minimal variance, useful for numerical stability
 
   Returns:
     sampled KL divergence between
@@ -122,21 +129,23 @@ def kl_gauss_ar1(key, z_mean_t, z_logvar_t, ar1_params):
   # Convert AR(1) parameters.
   # z_t = c + phi z_{t-1} + eps, eps \in N(0, noise var)
   ar1_mean = ar1_params['mean']
-  ar1_lognoisevar = ar1_params['lognvar']
+  ar1_lognoisevar = np.log(np.exp(ar1_params['lognvar'] + varmin))
   phi = np.exp(-np.exp(-ar1_params['logatau']))
+  # The process variance a function of noise variance, so I added varmin above.
+  # This affects log-likelihood funtions below, also.
   logprocessvar = ar1_lognoisevar - (np.log(1-phi) + np.log(1+phi))
 
   # Sample first AR(1) step according to process variance.
-  z0 = sample(next(skeys), z_mean_t[0], z_logvar_t[0])
-  logq = ll(z0, z_mean_t[0], z_logvar_t[0])
-  logp = ll(z0, ar1_mean, logprocessvar)
+  z0 = sample(next(skeys), z_mean_t[0], z_logvar_t[0], varmin)
+  logq = ll(z0, z_mean_t[0], z_logvar_t[0], varmin)
+  logp = ll(z0, ar1_mean, logprocessvar, 0.0)
   z_last = z0
 
   # Sample the remaining time steps with adjusted mean and noise variance.
   for z_mean, z_logvar in zip(z_mean_t[1:], z_logvar_t[1:]):
-    z = sample(next(skeys), z_mean, z_logvar)
-    logq += ll(z, z_mean, z_logvar)
-    logp += ll(z, ar1_mean + phi * z_last, ar1_lognoisevar)
+    z = sample(next(skeys), z_mean, z_logvar, varmin)
+    logq += ll(z, z_mean, z_logvar, varmin)
+    logp += ll(z, ar1_mean + phi * z_last, ar1_lognoisevar, 0.0)
     z_last = z
 
   kl = logq - logp
@@ -145,7 +154,7 @@ def kl_gauss_ar1(key, z_mean_t, z_logvar_t, ar1_params):
 
 # This function is used in the loss, which is already batch aware. But the
 # prior parameters are the same across batch, thus in_axes below.
-batch_kl_gauss_ar1 = vmap(kl_gauss_ar1, in_axes=(0, 0, 0, None))
+batch_kl_gauss_ar1 = vmap(kl_gauss_ar1, in_axes=(0, 0, 0, None, None))
 
 
 def diagonal_gaussian_params(key, n, mean=0.0, var=1.0):

lfads_tutorial/lfads.py
@@ -17,18 +17,11 @@
 
 
 from __future__ import print_function, division, absolute_import
-
-import h5py
-
-import distributions as dists
-
 import jax.numpy as np
-from jax import grad, jit, random, vmap
+from jax import jit, random, vmap
 import jax.flatten_util as flatten_util
-from jax.config import config
-#config.update("jax_platform_name", "Host") # CUDA / Jellyfish
-
-import utils
+import lfads_tutorial.distributions as dists
+import lfads_tutorial.utils as utils
 
 
 def sigmoid(x):
@@ -340,7 +333,8 @@ def lfads_decode(params, lfads_hps, key, ic_mean, ic_logvar, xenc_t, keep_rate):
   #    factors_{t-1} -> controller_t -> sample_t -> generator_t -> factors_t
   # is really one big loop and therefor one RNN.
   c = c0 = params['con']['h0']
-  g = g0 = dists.diag_gaussian_sample(next(skeys), ic_mean, ic_logvar)
+  g = g0 = dists.diag_gaussian_sample(next(skeys), ic_mean,
+                                      ic_logvar, lfads_hps['var_min'])
   f = f0 = np.zeros((lfads_hps['factors_dim'],))
   c_t = []
   ii_mean_t = []
@@ -353,7 +347,8 @@ def lfads_decode(params, lfads_hps, key, ic_mean, ic_logvar, xenc_t, keep_rate):
     c = gru(params['con'], c, cin)
     cout = affine(params['con_out'], c)
     ii_mean, ii_logvar = np.split(cout, 2, axis=0) # inferred input params
-    ii = dists.diag_gaussian_sample(next(skeys), ii_mean, ii_logvar)
+    ii = dists.diag_gaussian_sample(next(skeys), ii_mean,
+                                    ii_logvar, lfads_hps['var_min'])
     g = gru(params['gen'], g, ii)
     g = dropout(g, next(skeys), keep_rate)
     f = normed_linear(params['factors'], g)
@@ -441,17 +436,21 @@ def lfads_losses(params, lfads_hps, key, x_bxt, kl_scale, keep_rate):
   ic_post_mean_b = lfads['ic_mean']
   ic_post_logvar_b = lfads['ic_logvar']
   kl_loss_g0_b = dists.batch_kl_gauss_gauss(ic_post_mean_b, ic_post_logvar_b,
-                                            params['ic_prior'])
-  kl_loss_g0 = kl_scale * np.sum(kl_loss_g0_b) / B
-
+                                            params['ic_prior'],
+                                            lfads_hps['var_min'])
+  kl_loss_g0_prescale = np.sum(kl_loss_g0_b) / B  
+  kl_loss_g0 = kl_scale * kl_loss_g0_prescale
+  
   # KL - Inferred input
   ii_post_mean_bxt = lfads['ii_mean_t']
   ii_post_var_bxt = lfads['ii_logvar_t']
   keys = random.split(next(skeys), B)
   kl_loss_ii_b = dists.batch_kl_gauss_ar1(keys, ii_post_mean_bxt,
-                                          ii_post_var_bxt, params['ii_prior'])
-  kl_loss_ii = kl_scale * np.sum(kl_loss_ii_b) / B
-
+                                          ii_post_var_bxt, params['ii_prior'],
+                                          lfads_hps['var_min'])
+  kl_loss_ii_prescale = np.sum(kl_loss_ii_b) / B
+  kl_loss_ii = kl_scale * kl_loss_ii_prescale
+  
   # Log-likelihood of data given latents.
   lograte_bxt = lfads['lograte_t']
   log_p_xgz = np.sum(dists.poisson_log_likelihood(x_bxt, lograte_bxt)) / B
@@ -463,7 +462,9 @@ def lfads_losses(params, lfads_hps, key, x_bxt, kl_scale, keep_rate):
 
   loss = -log_p_xgz + kl_loss_g0 + kl_loss_ii + l2_loss
   all_losses = {'total' : loss, 'nlog_p_xgz' : -log_p_xgz,
-                'kl_g0' : kl_loss_g0, 'kl_ii' : kl_loss_ii, 'l2' : l2_loss}
+                'kl_g0' : kl_loss_g0, 'kl_g0_prescale' : kl_loss_g0_prescale,
+                'kl_ii' : kl_loss_ii, 'kl_ii_prescale' : kl_loss_ii_prescale,
+                'l2' : l2_loss}
   return all_losses
 
 

lfads_tutorial/optimize.py
@@ -22,8 +22,7 @@ import datetime
 import h5py
 
 import jax.numpy as np
-from jax import grad, jit, lax, random, vmap
-from jax import jacrev, jacfwd
+from jax import grad, jit, lax, random
 from jax.experimental import optimizers
 import jax.flatten_util as flatten_util
 
@@ -31,8 +30,8 @@ import matplotlib.pyplot as plt
 import numpy as onp  # original CPU-backed NumPy
 import sklearn
 
-import lfads
-import utils
+import lfads_tutorial.lfads as lfads
+import lfads_tutorial.utils as utils
 
 import time
 
@@ -49,12 +48,14 @@ def get_kl_warmup_fun(lfads_opt_hps):
 
   kl_warmup_start = lfads_opt_hps['kl_warmup_start']
   kl_warmup_end = lfads_opt_hps['kl_warmup_end']
+  kl_min = lfads_opt_hps['kl_min']
   kl_max = lfads_opt_hps['kl_max']
   def kl_warmup(batch_idx):
-    kl_warmup = np.where(batch_idx < kl_warmup_start, 0.0,
-                         kl_max * ((batch_idx - kl_warmup_start) /
-                                   (kl_warmup_end - kl_warmup_start)))
-    return np.where(batch_idx > kl_warmup_end, 1.0, kl_warmup)
+    progress_frac = ((batch_idx - kl_warmup_start) /
+                     (kl_warmup_end - kl_warmup_start))
+    kl_warmup = np.where(batch_idx < kl_warmup_start, kl_min,
+                         (kl_max - kl_min) * progress_frac + kl_min)
+    return np.where(batch_idx > kl_warmup_end, kl_max, kl_warmup)
   return kl_warmup
 
 
@@ -172,14 +173,12 @@ def optimize_lfads(key, init_params, lfads_hps, lfads_opt_hps,
   num_batches = lfads_opt_hps['num_batches']
   print_every = lfads_opt_hps['print_every']
   num_opt_loops = int(num_batches / print_every)
-  key, dtkeyg = utils.keygen(key, num_opt_loops) # data, train
-  key, dekeyg = utils.keygen(key, num_opt_loops) # data, eval
-  key, tkeyg = utils.keygen(key, num_opt_loops) # training
   params = optimizers.get_params(opt_state)
   for oidx in range(num_opt_loops):
     batch_idx_start = oidx * print_every
     start_time = time.time()
-    opt_state = optimize_lfads_core_jit(next(tkeyg), batch_idx_start,
+    key, tkey, dtkey, dekey = random.split(random.fold_in(key, oidx), 4)
+    opt_state = optimize_lfads_core_jit(tkey, batch_idx_start,
                                         print_every, update_fun, kl_warmup_fun,
                                         opt_state, lfads_hps, lfads_opt_hps,
                                         train_data)
@@ -189,17 +188,16 @@ def optimize_lfads(key, init_params, lfads_hps, lfads_opt_hps,
     params = optimizers.get_params(opt_state)
     batch_pidx = batch_idx_start + print_every
     kl_warmup = kl_warmup_fun(batch_idx_start)
-    
     # Training loss
     didxs = onp.random.randint(0, train_data.shape[0], batch_size)
     x_bxt = train_data[didxs].astype(onp.float32)
-    tlosses = lfads.lfads_losses_jit(params, lfads_hps, next(dtkeyg), x_bxt,
+    tlosses = lfads.lfads_losses_jit(params, lfads_hps, dtkey, x_bxt,
                                      kl_warmup, 1.0)
 
     # Evaluation loss
     didxs = onp.random.randint(0, eval_data.shape[0], batch_size)
     ex_bxt = eval_data[didxs].astype(onp.float32)
-    elosses = lfads.lfads_losses_jit(params, lfads_hps, next(dekeyg), ex_bxt,
+    elosses = lfads.lfads_losses_jit(params, lfads_hps, dekey, ex_bxt,
                                      kl_warmup, 1.0)
     # Saving, printing.
     all_tlosses.append(tlosses)