628cae109da8403d1e69b9e9519dead3b52d21be
David Sussillo <sussillo@google.com>
2020-03-08 10:26:02 -0700

    Created an experimental directory to separate more speculative ideas.

---
+++ experimental/contextual_integrator_rnn_tutorial/integrator.py
@@ -0,0 +1,101 @@
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+"""Routines for creating white noise and integrated white noise."""
+
+from __future__ import print_function, division, absolute_import
+from functools import partial
+import jax.numpy as np
+from jax import jit, vmap
+from jax import random
+import matplotlib.pyplot as plt
+import contextual_integrator_rnn_tutorial.utils as utils
+
+
+def build_input_and_target_pure_integration(input_params, key):
+  """Build white noise input and integration targets."""
+  bias_val, stddev_val, T, ntime = input_params
+  dt = T/ntime
+
+  # Create the white noise input.
+  key, skeys = utils.keygen(key, 3)
+  random_sample_1x2 = random.normal(next(skeys), shape=(1, 2))
+  bias_1x2 = bias_val * 2.0 * (random_sample_1x2 - 0.5)
+  stddev = stddev_val / np.sqrt(dt)
+  random_samples_tx2 = random.normal(next(skeys), shape=(ntime, 2))
+  noise_tx2 = stddev * random_samples_tx2
+  white_noise_tx2 = bias_1x2 + noise_tx2
+
+  # The context signal a hot one for the duration of the trial.
+  con1_tx2 = np.concatenate((np.ones((ntime,1)), np.zeros((ntime,1))), axis=1)
+  con2_tx2 = np.concatenate((np.zeros((ntime,1)), np.ones((ntime,1))), axis=1)
+  context = random.bernoulli(next(skeys))
+  context_tx2 = np.where(context, con1_tx2, con2_tx2)
+  
+  # * dt, intentionally left off to get output scaling in O(1).
+  targets_t = np.where(context,
+                       np.cumsum(white_noise_tx2[:,0]),
+                       np.cumsum(white_noise_tx2[:,1]))
+  inputs_tx4 = np.concatenate((white_noise_tx2, context_tx2), axis=1)
+  targets_tx1 = np.expand_dims(targets_t, axis=1)
+  return inputs_tx4, targets_tx1
+
+# Now batch it and jit.
+build_input_and_target = build_input_and_target_pure_integration
+
+#build_inputs_and_targets = vmap(build_input_and_target, in_axes=(None, 0))
+#build_inputs_and_targets_jit = jit(build_inputs_and_targets,
+#                                   static_argnums=(0,))
+
+def build_inputs_and_targets(input_params, keys):
+  f = partial(build_input_and_target, input_params)
+  f_vmap = vmap(f, (0,))
+  return f_vmap(keys)
+
+build_inputs_and_targets_jit = jit(build_inputs_and_targets, static_argnums=(0,))
+
+
+def plot_batch(ntimesteps, input_bxtxu, target_bxtxo=None, output_bxtxo=None,
+               errors_bxtxo=None, ntoplot=1):
+  """Plot some white noise / integrated white noise examples."""
+  plt.figure(figsize=(10,7))
+  plt.subplot(221)
+  plt.plot(input_bxtxu[0:ntoplot,:,0].T, 'b')
+  plt.plot(input_bxtxu[0:ntoplot,:,2].T, 'k')
+  plt.xlim([0, ntimesteps-1])
+  plt.ylabel('Noise')
+
+  plt.subplot(223)
+  plt.plot(input_bxtxu[0:ntoplot,:,1].T, 'b')
+  plt.plot(input_bxtxu[0:ntoplot,:,3].T, 'k')  
+  plt.xlim([0, ntimesteps-1])
+  plt.ylabel('Noise')
+
+  plt.subplot(222)
+  if output_bxtxo is not None:
+    plt.plot(output_bxtxo[0:ntoplot,:,0].T);
+    plt.xlim([0, ntimesteps-1]);
+  if target_bxtxo is not None:
+    plt.plot(target_bxtxo[0:ntoplot,:,0].T, '--');
+    plt.xlim([0, ntimesteps-1]);
+    plt.ylabel("Integration")
+  if errors_bxtxo is not None:
+    plt.subplot(224)
+    plt.plot(errors_bxtxo[0:ntoplot,:,0].T, '--');
+    plt.xlim([0, ntimesteps-1]);
+    plt.ylabel("|Errors|")
+  plt.xlabel('Timesteps')
+
+
---
+++ experimental/lfads_gmm/utils.py
@@ -0,0 +1,147 @@
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+"""Utility functions for the LFADS tutorial."""
+
+
+import h5py
+from jax import random
+import numpy as onp # original numpy
+import os
+
+MAX_SEED_INT = 10000000
+
+
+def split_data(data_b, train_fraction):
+  """Split the data into training and evaluation sets.
+
+  Arguments:
+    data_b: np array, data whose leading dimension is the batch
+
+  Returns:
+    2-tuple of trainind and eval splits
+  """
+  train_data_offset = 0
+  ndata = data_b.shape[0]
+  eval_data_offset = int(train_fraction * ndata)
+  train_data = data_b[train_data_offset:eval_data_offset]
+  eval_data = data_b[eval_data_offset:]
+
+  return train_data, eval_data
+
+
+def spikify_data(data_bxtxn, rng, dt=1.0, max_firing_rate=100):
+  """ Apply spikes to a continuous dataset whose values are between 0.0 and 1.0
+
+  Args:
+    data: nexamples x time x dim trials
+    dt: how often the data are sampled
+    max_firing_rate: the firing rate that is associated with a value of 1.0
+
+  Returns:
+    spikified_e: a list of length b of the data represented as spikes,
+      sampled from the underlying poisson process.
+    """
+  spikes_e = []
+  B, T, N = data_bxtxn.shape
+  for data_txn in data_bxtxn:
+    spikes = onp.zeros([T,N]).astype(onp.int)
+    for n in range(N):
+      f = data_txn[:,n]
+      s = rng.poisson(f*max_firing_rate*dt, size=T)
+      spikes[:,n] = s
+    spikes_e.append(spikes)
+  return onp.array(spikes_e)
+
+
+def merge_losses_dicts(list_of_dicts):
+  """List of dictionaries converted to dictionary of lists (numpy arrays).
+
+  Arguments:
+    list_of_dicts: list of dictionaries
+
+  Returns:
+    a single dictionary with merged keys and values of np arrays """
+  merged_d = {}
+  d = list_of_dicts[0]
+  for k in d:
+    merged_d[k] = []
+  for d in list_of_dicts:
+    for k in d:
+      merged_d[k].append(d[k])
+  for k in merged_d:
+    merged_d[k] = onp.array(merged_d[k])
+  return merged_d
+
+
+def average_lfads_batch(lfads_dict):
+  """Average over a run of lfads, for posterior sample and average.
+
+  Arguments:
+    lfads_dict: A dictionary of lfads hidden states, each value is a np array
+      with leading dim being batch.
+
+  Returns:
+    A dictionary with np arrays whose leading dim has been averaged over.
+  """
+  avg_dict = {}
+  for k in lfads_dict:
+    avg_dict[k] = onp.mean(lfads_dict[k], axis=0)
+  return avg_dict
+
+
+def ensure_dir(file_path):
+  """Make sure the directory exists, create if it does not."""
+  directory = os.path.dirname(file_path)
+  if not os.path.exists(directory):
+    os.makedirs(directory)
+
+
+def write_file(data_fname, data_dict, do_make_dir=False):
+  """Write a simple dictionary using h5py.
+
+  Arguments;
+    data_fname: name of file to save
+    data_dict: dictionary of values to save, each value in dict is a np array
+
+  """
+  try:
+    ensure_dir(data_fname)
+
+    with h5py.File(data_fname, 'w') as hf:
+      for k in data_dict:
+        hf.create_dataset(k, data=data_dict[k])
+        # add attributes
+  except IOError:
+    print("Cannot write % for writing." % data_fname)
+    raise
+
+
+def read_file(data_fname):
+  """Read a simple dictionary of np arrays using h5py.
+
+  Arguments:
+    data_fname: name of file to load
+
+  Returns:
+    dictionary of np arrays
+  """
+  try:
+    with h5py.File(data_fname, 'r') as hf:
+      data_dict = {k: onp.array(v) for k, v in hf.items()}
+      return data_dict
+  except IOError:
+    print("Cannot open %s for reading." % data_fname)
+    raise
